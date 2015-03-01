dc7775e734662d85862aa513c70acb349cc5953d
Will Storey <will@summercat.com>
2015-02-28 22:06:20 -0800

    twitlib: replace retweet tweet text with original
    this allows us to avoid truncation issues that occur with retweets
    such as when the original tweet is too long to encompass the
    'RT @screenname: ' portion. instead, we prepend the prefix portion
    ourselves to the original untruncated tweet

--- twitlib.tcl
+++ twitlib.tcl
@@ -84,6 +84,57 @@ proc ::twitlib::query {url {query_list {}} {http_method {}}} {
 	return [::json::json2dict $data]
 }
 
+# take status dict from a timeline and reformats it if necessary.
+#
+# in particular we replace retweeted tweets with the original tweet
+# but with 'RT @screenname:" prepended. this is to resolve issues
+# where retweeted tweets get truncated.
+#
+# note this assumes that with retweeting the tweet itself has no
+# new data (which seems to hold based on the API call to generate
+# a retweet).
+#
+# as well we replace newlines with a single space.
+proc ::twitlib::fix_status {status} {
+	set changed 0
+	set tweet [dict get $status text]
+
+	# if it has a retweet then as they can be truncated and lose
+	# data, especially urls, replace the tweet with the
+	# original tweet but add 'RT @name' to it.
+	if {[dict exists $status retweeted_status]} {
+		set rt_user [dict get $status retweeted_status user screen_name]
+		set rt_tweet [dict get $status retweeted_status text]
+		set tweet "RT @$rt_user: $rt_tweet"
+		set changed 1
+	}
+
+	# we can also apparently have newlines in tweets. replace them
+	# with a space.
+	# TODO: we could replace 2+ whitespace in a row with a single space.
+	set tweet_no_newlines [string map {\n " " \r ""} $tweet]
+	if {$tweet_no_newlines != $tweet} {
+		set changed 1
+		set tweet $tweet_no_newlines
+	}
+
+	if {$changed} {
+		set orig_tweet [dict get $status text]
+		dict set status text $tweet
+	}
+	return $status
+}
+
+# take a list of status dicts from a timeline and reformat them if necessary.
+proc ::twitlib::fix_statuses {statuses} {
+	set fixed_statuses [list]
+	foreach status $statuses {
+		set fixed_status [::twitlib::fix_status $status]
+		lappend fixed_statuses $fixed_status
+	}
+	return $fixed_statuses
+}
+
 # retrieve the latest unseen updates.
 #
 # we return a list of dicts. each dict represents a single
@@ -106,8 +157,11 @@ proc ::twitlib::get_unseen_updates {} {
 	# re-order - oldest to newest.
 	set result [lreverse $result]
 
+	# fix issues with truncation.
+	set statuses [::twitlib::fix_statuses $result]
+
 	set updates [list]
-	foreach status $result {
+	foreach status $statuses {
 		set screen_name [dict get $status user screen_name]
 		set id          [dict get $status id]
 		set created_at  [dict get $status created_at]
@@ -149,8 +203,11 @@ proc ::twitlib::get_unseen_mentions {} {
 	# re-order - oldest to newest.
 	set result [lreverse $result]
 
+	# fix issues with truncation.
+	set statuses [::twitlib::fix_statuses $result]
+
 	set updates [list]
-	foreach status $result {
+	foreach status $statuses {
 		set screen_name [dict get $status user screen_name]
 		set id          [dict get $status id]
 		set text        [dict get $status text]
